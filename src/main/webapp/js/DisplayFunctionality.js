c580fa7734e12a8341b7967528f003fa865fcc20
MerithzDTU <s195480@student.dtu.dk>
2020-06-11 13:38:44 +0200

    Made a function for buttons to toggle display of div content. Only implemented on BatchAdminPage for Creating Råvare and Produkt batches

---
+++ src/main/webapp/js/DisplayFunctionality.js
@@ -0,0 +1,11 @@
+
+//Funktion til knapper for at vise div content
+function toggleDivContent(id) {
+    var x = document.getElementById(id);
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
