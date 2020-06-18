9c3d6740941dd34e94411298fc47257641a4de9e
Colle <s195490@student.dtu.dk>
2020-06-18 12:57:42 +0200

    Hurtig fix for brugerform.js

--- src/main/webapp/js/FormKommandoer_BrugerForm.js
+++ src/main/webapp/js/FormKommandoer_BrugerForm.js
@@ -13,8 +13,6 @@ $(document).ready(function() {
 
 
     $('#vis-Brugere').on('click', function () {
-
-
         var bruger = {
             brugerCpr: $brugerCpr.val(),
             brugerNavn: $brugerNavn.val(),
@@ -23,91 +21,89 @@ $(document).ready(function() {
             brugerStatus: 'Aktiv',
         };
 
-        $('#vis-Brugere').on('click', function () {
+        $('#brugere').html('');
 
-            $('#brugere').html('');
+        $.ajax({
+            type: 'GET',
+            url: 'api/bruger/vis',
+            contentType: "application/json; charset=utf-8",
+            data: bruger,
+            success: function (data) {
+                $.each(data, function (i, bruger) {
+                    $brugere.append('<li>Bruger CPR: ' + bruger.cprNr + ', Bruger navn: ' + bruger.brugerNavn + ', Bruger initialer: ' + bruger.ini + ', Bruger rolle: ' + bruger.rolle + ', Bruger status: ' + bruger.brugerStatus + '</li><br>');
+                });
+            },
+            error: function () {
+                alert('Fejl ved indlæsning af brugere');
+            },
+        });
+    });
 
+    $('#opret-Bruger').on('click', function () {
+        var bruger = {
+            brugerCpr: $brugerCpr.val(),
+            brugerNavn: $brugerNavn.val(),
+            brugerIni: $brugerIni.val(),
+            brugerRolle: $brugerRolle.val(),
+            brugerStatus: 'Aktiv',
+        };
+
+        if (bruger.brugerCpr.length === 10) {
             $.ajax({
-                type: 'GET',
-                url: 'api/bruger/vis',
+                type: 'POST',
+                url: 'api/bruger/opret',
+                dataType: "json",
                 contentType: "application/json; charset=utf-8",
                 data: bruger,
-                success: function (data) {
-                    $.each(data, function (i, bruger) {
-                        $brugere.append('<li>Bruger CPR: ' + bruger.cprNr + ', Bruger navn: ' + bruger.brugerNavn + ', Bruger initialer: ' + bruger.ini + ', Bruger rolle: ' + bruger.rolle + ', Bruger status: ' + bruger.brugerStatus + '</li><br>');
-                    });
+                success: function () {
+                    alert('Bruger oprettet!');
+                    $brugere.append('<li>Bruger CPR: ' + bruger.brugerCpr + ', Bruger navn: ' + bruger.brugerNavn + ', Bruger initialer: ' + bruger.brugerIni + ', Bruger rolle: ' + bruger.brugerRolle + ', Bruger status: ' + bruger.brugerStatus + '</li>');
                 },
                 error: function () {
-                    alert('Fejl ved indlæsning af brugere');
-                },
+                    alert('Fejl ved oprettelse af bruger');
+                }
             });
-        });
+        } else {
+            alert('CPR-nummer er for kort')
+        }
+    });
 
-        $('#opret-Bruger').on('click', function () {
-            var bruger = {
-                brugerCpr: $brugerCpr.val(),
-                brugerNavn: $brugerNavn.val(),
-                brugerIni: $brugerIni.val(),
-                brugerRolle: $brugerRolle.val(),
-                brugerStatus: 'Aktiv',
-            };
+    $('#slet-Brugere').on('click', function () {
+        var bruger = {
+            brugerCpr: $sletCPR.val(),
+            brugerNavn: '',
+            brugerIni: '',
+            brugerRolle: '',
+            brugerStatus: '',
+        };
 
-            if (bruger.brugerCpr.length === 10) {
-                $.ajax({
-                    type: 'POST',
-                    url: 'api/bruger/opret',
-                    dataType: "json",
-                    contentType: "application/json; charset=utf-8",
-                    data: bruger,
-                    success: function () {
-                        alert('Bruger oprettet!');
-                        $brugere.append('<li>Bruger CPR: ' + bruger.brugerCpr + ', Bruger navn: ' + bruger.brugerNavn + ', Bruger initialer: ' + bruger.brugerIni + ', Bruger rolle: ' + bruger.brugerRolle + ', Bruger status: ' + bruger.brugerStatus + '</li>');
-                    },
-                    error: function () {
-                        alert('Fejl ved oprettelse af bruger');
-                    }
-                });
-            } else {
-                alert('CPR-nummer er for kort')
-            }
-        });
+        if (bruger.brugerCpr.length === 10) {
+            $.ajax({
+                type: 'POST',
+                url: 'api/bruger/slet',
+                contentType: "application/json; charset=utf-8",
+                data: bruger,
+                success: function () {
+                    alert('Bruger slettet!');
+                },
+                error: function (e) {
+                    console.log(e);
+                    alert(e);
+                }
+            });
+        } else {
+            alert('CPR-nummer er for kort')
+        }
+    });
 
-        $('#slet-Brugere').on('click', function () {
-            var bruger = {
-                brugerCpr: $sletCPR.val(),
-                brugerNavn: '',
-                brugerIni: '',
-                brugerRolle: '',
-                brugerStatus: '',
-            };
-
-            if (bruger.brugerCpr.length === 10) {
-                $.ajax({
-                    type: 'POST',
-                    url: 'api/bruger/slet',
-                    contentType: "application/json; charset=utf-8",
-                    data: bruger,
-                    success: function () {
-                        alert('Bruger slettet!');
-                    },
-                    error: function (e) {
-                        console.log(e);
-                        alert(e);
-                    }
-                });
-            } else {
-                alert('CPR-nummer er for kort')
-            }
-        });
-
-        $('#rediger-Bruger').on('click', function () {
-            var redigerbruger = {
-                brugerCpr: $redigerCpr.val(),
-                brugerNavn: $redigerNavn.val(),
-                brugerIni: $redigerIni.val(),
-                brugerRolle: $redigerRolle.val(),
-                brugerStatus: 'Aktiv',
-            };
+    $('#rediger-Bruger').on('click', function () {
+        var redigerbruger = {
+            brugerCpr: $redigerCpr.val(),
+            brugerNavn: $redigerNavn.val(),
+            brugerIni: $redigerIni.val(),
+            brugerRolle: $redigerRolle.val(),
+            brugerStatus: 'Aktiv',
+        };
 
         $.ajax({
             type: 'POST',
